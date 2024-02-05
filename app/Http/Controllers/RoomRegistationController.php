c69393fae9866b941646535928680d3d2a0e07e4
Rasyidin17 <maduhur101@gmail.com>
2024-02-05 09:40:57 +0700

    controller model

---
+++ app/Http/Controllers/RoomRegistationController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RoomRegistation;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RoomRegistationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RoomRegistation $roomRegistation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RoomRegistation $roomRegistation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, RoomRegistation $roomRegistation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RoomRegistation $roomRegistation)
+    {
+        //
+    }
+}

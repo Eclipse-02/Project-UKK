5df9963d3c3fdfc5dcafcc62b562be5c05b040ba
Ecliße <rafaumar22@gmail.com>
2024-02-15 15:20:34 +0700

    Fix ' after id on url

--- resources/views/scaffolds/facilities/action.blade.php
+++ resources/views/scaffolds/facilities/action.blade.php
@@ -2,7 +2,7 @@
     @csrf
     @method('DELETE')
 
-    <a class="btn btn-icon btn-bg-light btn-active-color-primary btn-md me-1" href="{{ route('facilities.show', $id) }}'" >
+    <a class="btn btn-icon btn-bg-light btn-active-color-primary btn-md me-1" href="{{ route('facilities.show', $id) }}" >
         <!--begin::Svg Icon | path: assets/media/icons/duotune/general/gen004.svg-->
         <span class="svg-icon svg-icon-muted svg-icon-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
         <path d="M21.7 18.9L18.6 15.8C17.9 16.9 16.9 17.9 15.8 18.6L18.9 21.7C19.3 22.1 19.9 22.1 20.3 21.7L21.7 20.3C22.1 19.9 22.1 19.3 21.7 18.9Z" fill="black"/>
